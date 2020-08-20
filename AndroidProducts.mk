a66b49cbbd196b31727874dac37debd75fa113f9
baljeethakur <v21.vky@gmail.com>
2020-08-20 09:49:23 +0000

    Derpify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosip_guacamole.mk
+    $(LOCAL_DIR)/derp_guacamole.mk
--- derp_guacamole.mk
+++ derp_guacamole.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/oneplus/guacamole/device.mk)
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosip_guacamole
+PRODUCT_NAME := derp_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus 7 Pro
